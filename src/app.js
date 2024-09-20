7580e431e6ab948045ffb9f9d96596ab16f4f421
Sidarth999 <b200074@nitsikkim.ac.in>
2024-09-20 11:30:28 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes import
+import router from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users", router)
+
+
 export { app }
