8b0634042d568989a5192bbd777255d5622ec56f
Sidarth999 <b200074@nitsikkim.ac.in>
2024-09-20 13:01:14 +0530

    fix all Register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // retrun res / error
 
     const {fullName, email, username, password} = req.body
-    console.log("Email: ", email);
+    // console.log("Email: ", email);
 
     // if(fullName === ""){
     //     throw new ApiError(400, "fullname is required")
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -40,14 +40,19 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path
 
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-
+    
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
