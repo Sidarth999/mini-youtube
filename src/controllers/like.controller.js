0f86e0100e09c6198ff589b48e7d8d38f9d340d7
Sidarth999 <b200074@nitsikkim.ac.in>
2024-10-03 17:47:31 +0530

    All controllers fixed successfully

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -100,9 +100,76 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {isLiked: true}, "Video Liked Successfully"))
 })
 
+//TODO: get all liked videos
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const likedVideosAggregate = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                        }
+                    },
+                    {
+                        $unwind: "$ownerDetails"
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$likedVideo"
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likedVideo: {
+                    _id: 1,
+                    "videoFile.url" : 1,
+                    "thumbnail.url": 1,
+                    owner: 1,
+                    title: 1,
+                    description : 1,
+                    views: 1,
+                    duration: 1,
+                    createdAt: 1,
+                    isPublished : 1,
+                    ownerDetails: {
+                        username: 1,
+                        fullName: 1,
+                        "avatar.url": 1
+                    }
+                }
+            }
+        }
+    ])
 
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            likedVideosAggregate,
+            "Liked videos fetched successfully"
+        )
+    )
 })
 
 export {
